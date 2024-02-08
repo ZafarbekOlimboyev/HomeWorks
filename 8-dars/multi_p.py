5c5c483b2b1a91a5bb991e653989bf2fd8225c10
Your Name <zafarbekolimboyev07@gmail.com>
2024-02-08 11:49:24 +0500

    .

--- 8-dars/multi_p.py
+++ 8-dars/multi_p.py
@@ -1,7 +1,9 @@
 from pdf2image import convert_from_path
 from multiprocessing import Pool
+from time import perf_counter
 
 def pdf_to_jpg(pdf_data):
+    start = perf_counter()
     pdf_path, output_folder, pdf_number = pdf_data
     images = convert_from_path(pdf_path)
 
@@ -9,6 +11,8 @@ def pdf_to_jpg(pdf_data):
         output_path = f"{output_folder}/{pdf_number}-mavzu_{i}.jpg"
         image.save(output_path, "JPEG")
         print(f"{pdf_number}-mavzu_{i}.jpg saved")
+    end  = perf_counter()
+    print(end - start, "sekund")
 
 def process_pdf(pdfs):
     with Pool() as pool:
@@ -20,3 +24,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# 13 pdf -->  13.003614452994952 sekund
--- 8-dars/single_p.py
+++ 8-dars/single_p.py
@@ -1,4 +1,5 @@
 from pdf2image import convert_from_path
+from time import perf_counter
 
 def pdf_to_jpg(pdf_path, output_folder,pdf_number):
     images = convert_from_path(pdf_path)
@@ -9,8 +10,13 @@ def pdf_to_jpg(pdf_path, output_folder,pdf_number):
         print(f"{pdf_number}-mavzu_{i}.jpg saved")
 
 def pdf_p(out_floder):
+    start = perf_counter()
     for i in range(1,13):
         pdf_to_jpg(f"pdfs/{i}-mavzu.pdf",out_floder,i)
+    end = perf_counter()
+    print(f"FINISH\n{end - start : .3f} sekund")
 
 if __name__ == "__main__":
     pdf_p("JPEGS_SP")
+
+# 13 pdf -->  52.336  sekund
